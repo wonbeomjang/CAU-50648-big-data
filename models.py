ecbd5b39af907475e2ef5025c5f34450b9f8e7f3
wonbeomjang <jtiger958@gmail.com>
2022-11-27 04:40:46 +0900

    change embedding size

--- models.py
+++ models.py
@@ -6,8 +6,8 @@ class PogModel(nn.Module):
     def __init__(self):
         super(PogModel, self).__init__()
         self.backbone = resnet50(weights=ResNet50_Weights.IMAGENET1K_V1)
-        self.backbone.fc = nn.Linear(512 * 4, 8)
-        self.classifier = nn.Linear(8, 1)
+        self.backbone.fc = nn.Linear(512 * 4, 768)
+        self.classifier = nn.Linear(768, 1)
 
     def forward(self, x):
         x = self.backbone(x)
